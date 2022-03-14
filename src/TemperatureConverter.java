0dc1d459adbef1f2713a5c4783e64c612a7cfb92
sonjjj <wwldn3912@naver.com>
2022-03-14 22:18:53 +0900

    finish

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -5,12 +5,10 @@ public class TemperatureConverter {
 	public static void main(String[] args) {
 		System.out.print("Enter Fahrenheit temp: ");
 		
-		Double F, C;
-		
 		Scanner sc = new Scanner(System.in);
 		
-		F = sc.nextDouble();
-		C = (F-32)*5/9;
+		Double F = sc.nextDouble();
+		Double C = (F-32)*5/9;
 		
 		System.out.println("Converted celsius temp: " + C);
 		
