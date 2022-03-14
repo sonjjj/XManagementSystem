73d2301f0119c72abe97dee1959a05697fdd8e03
sonjjj <wwldn3912@naver.com>
2022-03-14 20:07:36 +0900

    removed menumanager.java and added playermanager.java

---
+++ src/PlayerManager.java
@@ -0,0 +1,12 @@
+
+public class PlayerManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Players");
+		System.out.println("2. Delete Players");
+		System.out.println("3. Edit Players");
+		System.out.println("4. View Players");
+		System.out.println("5. Exit");
+	}
+
+}
