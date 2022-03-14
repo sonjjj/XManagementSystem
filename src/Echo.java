49890e10edc1849659c3e451934954a14e718cc6
sonjjj <wwldn3912@naver.com>
2022-03-14 22:18:40 +0900

    finish

--- src/Echo.java
+++ src/Echo.java
@@ -4,11 +4,10 @@ public class Echo {
 
 	public static void main(String[] args) {
 		System.out.print("Input the Sentence: ");
-		
-		String str;		
+			
 		Scanner sc = new Scanner(System.in);
-		str = sc.nextLine();
-		System.out.println("" + str);
+		String str = sc.nextLine();
+		System.out.println(str);
 		sc.close();
 	}
 
