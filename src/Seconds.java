661e36f53c7b3dc1765e8b90c280b3b2d6f72fed
sonjjj <wwldn3912@naver.com>
2022-03-21 01:06:07 +0900

    Added <input.close();> to remove error.

--- src/Seconds.java
+++ src/Seconds.java
@@ -15,9 +15,17 @@ public class Seconds {
 		int s = seconds % 60;
 		int m = minutes % 60;
 		int h = hours % 24;
-	
-		System.out.print(seconds + "초는 " + days + "일 " + h + "시간 " + m + "분 " + s + "초");
 		
+		input.close();
+	
+		System.out.println(seconds + "초는 " + days + "일 " + h + "시간 " + m + "분 " + s + "초");
+		
+		if (days > 365) {
+			System.out.println(seconds + "초는 1년이 넘습니다.");
+		}
+		else {
+			System.out.println(seconds + "초는 1년이 넘지 않습니다.");
+		}
 	}
 	
 }
