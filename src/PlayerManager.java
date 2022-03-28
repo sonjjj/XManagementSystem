185111631f1a9bd36440feb3a217ec83a1bb3a7a
sonjjj <wwldn3912@naver.com>
2022-03-28 23:02:48 +0900

    methods

--- src/PlayerManager.java
+++ src/PlayerManager.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class PlayerManager {
 
 	public static void main(String[] args) {
-		
 		int num = 0;
 		Scanner input = new Scanner (System.in);
-		
+
 		while (num != 6) {
 			System.out.println("\n1. Add Players");
 			System.out.println("2. Delete Players");
@@ -14,39 +13,58 @@ public class PlayerManager {
 			System.out.println("4. View Players");
 			System.out.println("5. Show a Menu");
 			System.out.println("6. Exit");
-			System.out.println("\nSelect one number between 1 - 6: ");
+			System.out.println("Select one number between 1 - 6: ");
 			num = input.nextInt();
-			
-			switch(num) {
-			case 1:
-				System.out.println("Player ID: ");
-				int playerID = input.nextInt();
-				
-				System.out.println("Player Name: ");
-				String playerName = input.next();
-				
-				System.out.println("Player E-mail Adress: ");
-				String playerEmail = input.next();
-				
-				input.nextLine();
-				
-				System.out.println("Player Phone Number: ");
-				int playerPhone = input.nextInt();
-				
-				break;
-			case 2:
-				System.out.println("Player ID: ");
-				int playerID2 = input.nextInt();
-				break;
-			case 3:
-				System.out.println("Player ID: ");
-				int playerID3 = input.nextInt();
-				break;
-			case 4:
-				System.out.print("Player ID: ");
-				int PlayerID4 = input.nextInt();
-				break;
+
+			if (num == 1) {
+				addStudent();
 			}
-		}	
+			else if (num == 2) {
+				deleteStudent();
+			}
+			else if (num == 3) {
+				editStudent();
+			}
+			else if (num == 4) {
+				viewStudent();
+			}
+			else {
+				continue;
+			}
+		}
+	}
+	
+	public static void addStudent() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Student ID: ");
+		int studentId = input.nextInt();
+		
+		System.out.print("Student Name: ");
+		String studentName = input.next();
+		
+		System.out.print("Student email adress: ");
+		String studentEmail = input.next();
+		
+		System.out.print("Student Phone number: ");
+		String studentPhone = input.next();
+	}
+	
+	public static void deleteStudent() {
+        Scanner input = new Scanner(System.in);
+		System.out.print("Student ID: ");
+		int studentId = input.nextInt();
+	}
+		
+	public static void editStudent() {
+        Scanner input = new Scanner(System.in);
+		System.out.print("Student ID: ");
+		int studentId = input.nextInt();
+	}
+	
+	public static void viewStudent() {
+        Scanner input = new Scanner(System.in);
+		System.out.print("Student ID: ");
+		int studentId = input.nextInt();
 	}
 }
