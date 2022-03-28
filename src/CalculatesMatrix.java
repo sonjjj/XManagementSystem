9ce009cbc6a00c51be70d26b5937250f0e3e7035
sonjjj <wwldn3912@naver.com>
2022-03-28 22:51:00 +0900

    calculates the product of matrix A and matrix B

---
+++ src/CalculatesMatrix.java
@@ -0,0 +1,27 @@
+
+public class CalculatesMatrix {
+
+	public static void main(String[] args) {
+		int[][] matrix1 = {{1, 0, 2}, {-1, 3, 1}};
+		int[][] matrix2 = {{3, 1}, {2, 1}, {1, 0}};
+		int[][] result = new int[2][2];
+		
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++) {
+				for(int k = 0; k < 3; k++) {
+					result[i][j] += matrix1[i][k] * matrix2[k][j];
+				}
+			}
+		}
+		
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+		System.out.println();
+		
+		}
+
+	}
+
+}
