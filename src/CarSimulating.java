2899ea628dcfae5bfee62e74a8064d5c72a54f14
sonjjj <wwldn3912@naver.com>
2022-04-04 22:31:50 +0900

    Edited comments

--- src/CarSimulating.java
+++ src/CarSimulating.java
@@ -8,19 +8,19 @@ public class CarSimulating {
 		
 		System.out.println("Car's year model: (e.g. 2015)");
 		int num1 = input.nextInt(); // 제작연도를 입력 받아
-		car.getYear(num1); // car 클래스에 저장
+		car.getYear(num1); // car 클래스(생성자)에 저장
 		
 		System.out.println("Car's maker: (e.g. 'Honda')");
 		String num2 = input.next(); // 제조회사를 입력 받아
-		car.getMaker(num2); // car 클래스에 저장
+		car.getMaker(num2); // car 클래스(생성자)에 저장
 		
 		System.out.println("Car's maximum speed: (e.g. 85.0)");
 		double num3 = input.nextDouble(); // 최대 속도를 입력 받아
-		car.getSpeed(num3); // car 클래스에 저장
+		car.getSpeed(num3); // car 클래스(생성자)에 저장
 		
-		System.out.println("차의 제작연도: " + car.year); // 제작연도 리턴
-		System.out.println("차의 제조회사: " + car.maker); // 제조회사 리턴
-		System.out.println("차의 최대속도: " + car.speed); // 최대 속도 리턴
+		System.out.println("차의 제작연도: " + car.year); // 생성자에 저장한 제작연도 리턴
+		System.out.println("차의 제조회사: " + car.maker); // 생성자에 저장한 제조회사 리턴
+		System.out.println("차의 최대속도: " + car.speed); // 생성자에 저장한 최대 속도 리턴
 		
 		System.out.println("차의 속도를 모의실험해봅시다");
 		System.out.println("얼마로 달릴까요?");
