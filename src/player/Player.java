ca31f6a36922482fbe5fce6798cd535720b6a539
sonjjj <wwldn3912@naver.com>
2022-04-26 00:33:31 +0900

    Player Class

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,10 +1,15 @@
+package player;
+
+import java.util.Scanner;
+
 public class Player {
-	String name;
-	int id;
-	String email;
-	String phone;
+    protected PlayerPosition position = PlayerPosition.Wingspiker; // 초기값 지정
+	protected String name;
+	protected int id;
+	protected int point;
+	protected String team;
 	
-	public Player() { 
+	public Player() {  
 	}
 	
 	public Player(String name, int id) {
@@ -12,17 +17,73 @@ public class Player {
 		this.id = id;
 	}
 	
-	public Player(String name, int id, String email, String phone) {
+	public Player(String name, int id, int point, String team) {
 		this.name = name;
 		this.id = id;
-		this.email = email;
-		this.phone = phone;
+		this.point = point;
+		this.team = team;
+	}
+	
+	public PlayerPosition getPosition() {
+		return position;
+	}
+
+	public void setPosition(PlayerPosition position) {
+		this.position = position;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
 	}
 	
 	public void printInfo() { // static 빠진이유: 메모리에 바로 올릴 필요 없음
-		System.out.println("name: " + name + "| id: " + id + "| email: " + email + "| phone: " + phone);
+		System.out.println("name: " + name + " | id: " + id + " | points: " + point + " | team: " + team);
 	}
 	
-	// 함수 하나 더 만들기
+	public void getUserInput(Scanner input) {
+		System.out.print("Player ID: ");
+		int id = input.nextInt();
+		this.setId(id); // 내 객체에 대한 각각의 값들이 input 통해 들어감
+		 
+		System.out.print("Player Name: ");
+		String name = input.next();
+		this.setName(name);
+		
+		System.out.print("Player points: ");
+		int point = input.nextInt();
+		this.setPoint(point);
+		
+		System.out.print("Player team: ");
+		String team = input.next();
+		this.setTeam(team);
+	}
 
 }
