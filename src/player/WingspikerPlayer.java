0dc9f2a0bd69cb955daec5298ccd84bdee1cf2f7
sonjjj <wwldn3912@naver.com>
2022-05-01 00:56:17 +0900

    Added Question & position

--- src/player/WingspikerPlayer.java
+++ src/player/WingspikerPlayer.java
@@ -3,6 +3,10 @@ package player;
 import java.util.Scanner;
 
 public class WingspikerPlayer extends Player {
+	
+	public WingspikerPlayer(PlayerPosition position) {
+		super(position);
+	}
 
 	public void getUserInput(Scanner input) {
 		System.out.print("Player ID: ");
@@ -13,20 +17,20 @@ public class WingspikerPlayer extends Player {
 		String name = input.next();
 		this.setName(name);
 		
-		char answer = 'x';
-		while (answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N')
+		char answer1 = 'x';
+		while (answer1 != 'y' && answer1 != 'Y' && answer1 != 'n' && answer1 != 'N')
 		{
-			System.out.print("Does your offensing points over 50 points? (Y/N)");
-			answer = input.next().charAt(0);
+			System.out.print("? Are you on the starting list ?");
+			answer1 = input.next().charAt(0);
 			
-			if (answer == 'y' || answer == 'Y') {
+			if (answer1 == 'y' || answer1 == 'Y') {
 				System.out.print("Player offensing points: ");
 				int point = input.nextInt();
 				this.setPoint(point);
 				break;
 			}
 			
-			else if (answer == 'n' || answer == 'N') {
+			else if (answer1 == 'n' || answer1 == 'N') {
 				this.setPoint(0);
 				break;
 			}
@@ -35,8 +39,49 @@ public class WingspikerPlayer extends Player {
 			}
 		}
 		
-		System.out.print("Player team: ");
-		String team = input.next();
-		this.setTeam(team);
+		char answer2 = 'x';
+		while (answer2 != 'y' && answer2 != 'Y' && answer2 != 'n' && answer2 != 'N')
+		{
+			System.out.print("? Do you work for a primere v-league ?");
+			answer2 = input.next().charAt(0);
+			
+			if (answer2 == 'y' || answer2 == 'Y') {
+				System.out.print("Player team: ");
+				String team = input.next();
+				this.setTeam(team);
+				break;
+			}
+			
+			else if (answer2 == 'n' || answer2 == 'N') {
+				this.setTeam("-businessteam-");
+				break;
+			}
+			
+			else {
+			}
+		}
 	}
+	public void printInfo() { // 추가 정보 출력
+		// 선수 종류에 따라 바뀔 예정	
+			String pos = "none";
+			
+			switch(this.position) {
+			case Wingspiker:
+				pos = "Wing";
+				break;
+			case Setter:
+				pos = "Setter";
+				break;
+			case Center:
+				pos = "Center";
+				break;
+			case Libero:
+				pos = "Libero";
+				break;
+			default: //모든 케이스가 아닌 경우
+				
+			}
+			
+			System.out.println("position: " + pos + " | name: " + name + " | id: " + id + " | Spike points: " + point + " | team: " + team);
+		}
 }
