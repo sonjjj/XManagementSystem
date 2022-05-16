fb0e91e1d8bb68323dfe8f0a1c9db13bd45292f7
sonjjj <wwldn3912@naver.com>
2022-05-16 23:26:07 +0900

    Added methods

--- src/player/PlayerInput.java
+++ src/player/PlayerInput.java
@@ -2,11 +2,13 @@ package player;
 
 import java.util.Scanner;
 
+import exceptions.IDException;
+
 public interface PlayerInput { // 자식 클래스 공통 메소드(선언) 공유하는 Interface 추가
 		
 	public int getId();
 	
-	public void setId(int id);
+	public void setId(int id) throws IDException;
 	
 	public void setName(String name);
 	
@@ -17,5 +19,13 @@ public interface PlayerInput { // 자식 클래스 공통 메소드(선언) 공유하는 Interfa
 	public void getUserInput(Scanner input);
 	
 	public void printInfo();
+	
+	public void setPlayerID(Scanner input);
+	
+	public void setPlayerName(Scanner input);
+	
+	public void setPlayerPoint(Scanner input);
+	
+	public void setPlayerTeam(Scanner input);
 	
 }
